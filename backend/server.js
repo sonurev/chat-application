371611146cbb88f1c3aefcf230c340946dcfdeb2
sonu meena <kumarsonu123karauli@gmail.com>
2024-03-15 09:25:53 +0530

    deplyoment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
 app.use(cookieParser());
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -12,6 +12,8 @@ import { app,server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 
@@ -20,10 +22,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   //root route http://localhost:5000
-//   res.send("hello world hey how are you");
-// });
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
 
 server.listen(PORT, () => {
   connectToMongodb();
